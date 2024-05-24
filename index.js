151d24b9839b91e9d9e447e70c45252ad7058ff9
alftantay <alftantay@gmail.com>
2024-05-24 08:35:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const btnLike1 = document.getElementById("btnLike1")
 const count1 = document.getElementById("count1")
 
 function likeEvent1() {
-  let countLike1 = parseInt(count1.value) + 0
+  let countLike1 = parseInt(count1.value) + 1
   count1.textContent = countLike1.toString()
 }
 btnLike1.addEventListener("click", likeEvent1)
