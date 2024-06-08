05c888dbebd269bb9cb6933824a04d55ab1a3396
alftantay <alftantay@gmail.com>
2024-06-07 19:36:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,5 @@ function clickLike2(){
 }
 btnLike2.addEventListener("click",clickLike2)
 
-const submit = document.getElementById("submit")
-const comment = document.getElementById("comment")
-const commentbox = document.getElementById("commentbox")
-function submitComment(){
-   
-    commentbox.textContent += comment.value.toString() + "\n"
-     comment.value=""
-}
-submit.addEventListener("click",submitComment)
+
 
